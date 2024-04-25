3c36f10efa7d0ee8cfc09932da4b8f2551069ecf
diega <diegochiola@gmail.com>
2024-04-25 16:02:00 +0200

    Media-queries: homepage and contact done.

--- js/contact.js
+++ js/contact.js
@@ -99,3 +99,16 @@ window.addEventListener("load", function () {
     }
   });
 });
+
+const nav = document.querySelector(".nav-bar");
+const openMenu = document.querySelector("#open-menu");
+const closeMenu = document.querySelector("#close-menu");
+console.log(openMenu);
+openMenu.addEventListener("click", () => {
+    nav.classList.add("show");
+});
+
+closeMenu.addEventListener("click", () => {
+    nav.classList.toggle("show");
+    console.log("hola");
+});
--- js/project.js
+++ js/project.js
@@ -60,3 +60,15 @@ document.addEventListener("DOMContentLoaded", function () {
   getJsonToCards();
 });
 
+const nav = document.querySelector(".nav-bar");
+const openMenu = document.querySelector("#open-menu");
+const closeMenu = document.querySelector("#close-menu");
+console.log(openMenu);
+openMenu.addEventListener("click", () => {
+    nav.classList.add("show");
+});
+
+closeMenu.addEventListener("click", () => {
+    nav.classList.toggle("show");
+    console.log("hola");
+});
